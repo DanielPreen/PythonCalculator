c2a02761c546d6b3ad300d0181e259f7f89daa08
DanielPreen <daniel.preen.dev@gmail.com>
2021-12-12 10:07:19 +0000

    Added more operations

--- index.py
+++ index.py
@@ -1,7 +1,22 @@
 operation = input()
+
 a = input()
 b = input()
 
+exitMessage = "Press any key to exit: "
+
 if operation == "+":
     print(int(a)+(int(b)))
-    input("Press any key to continue")
+    input(exitMessage)
+
+if operation == "-":
+    print(int(a)-(int(b)))
+    input(exitMessage)
+
+if operation == "*":
+    print(int(a)*(int(b)))
+    input(exitMessage)
+
+if operation == "/":
+    print(int(a)/(int(b)))
+    input(exitMessage)
